52f31389fc863632094964fdb156d4d108627094
ThaiVanHandSome <danitbadao1234@gmail.com>
2024-08-22 11:55:42 +0700

    done Paginate Component

--- src/components/Paginate/Paginate.tsx
+++ src/components/Paginate/Paginate.tsx
@@ -1,5 +1,64 @@
-import React from 'react'
+import clsx from 'clsx'
+interface Props {
+  readonly page: number
+  readonly setPage: React.Dispatch<React.SetStateAction<number>>
+  readonly pageSize: number
+}
 
-export default function Paginate() {
-  return <div>Paginate</div>
+const RANGE = 2
+export default function Paginate({ page, setPage, pageSize }: Props) {
+  const renderPagination = () => {
+    let dotAfter = false
+    let dotBefore = false
+    const renderDotBefore = () => {
+      if (!dotBefore) {
+        dotBefore = true
+        return <div className='bg-white rounded px-3 py-2 shadow-sm mx-2'>...</div>
+      }
+      return null
+    }
+
+    const renderDotAfter = () => {
+      if (!dotAfter) {
+        dotAfter = true
+        return <div className='bg-white rounded px-3 py-2 shadow-sm mx-2'>...</div>
+      }
+      return null
+    }
+    return Array(pageSize)
+      .fill(1)
+      .map((_, index) => {
+        const pageNumber = index + 1
+        if (page <= RANGE * 2 + 1 && pageNumber > page + RANGE && pageNumber < pageSize - RANGE + 1) {
+          return renderDotAfter()
+        } else if (page > RANGE * 2 + 1 && page < pageSize - RANGE * 2) {
+          if (pageNumber < page - RANGE && pageNumber > RANGE) {
+            return renderDotBefore()
+          } else if (pageNumber > page + RANGE && pageNumber <= pageSize - RANGE) {
+            return renderDotAfter()
+          }
+        } else if (page >= pageSize - RANGE * 2 && pageNumber < page - RANGE && pageNumber > RANGE) {
+          return renderDotBefore()
+        }
+        return (
+          <button
+            key={pageNumber}
+            className={clsx('bg-white border-2 rounded px-3 py-2 shadow-sm mx-2 cursor-pointer', {
+              'border-cyan-500': pageNumber === page,
+              'border-transparent': pageNumber !== page
+            })}
+          >
+            {pageNumber}
+          </button>
+        )
+      })
+  }
+
+  return (
+    <div className='flex flex-wrap mt-6 justify-center'>
+      <button className='bg-white rounded px-3 py-2 shadow-sm mx-2 cursor-pointer'>Prev</button>
+      {renderPagination()}
+      <button className='bg-white rounded px-3 py-2 shadow-sm mx-2 cursor-pointer'>Next</button>
+    </div>
+  )
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -4,9 +4,14 @@ import useQueryParams from 'src/hooks/useQueryParams'
 import { getProducts } from 'src/apis/product.api'
 import SortProductList from 'src/pages/ProductList/SortProductList'
 import Product from 'src/pages/ProductList/Product/Product'
+import { useState } from 'react'
+import Paginate from 'src/components/Paginate'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
+
+  const [page, setPage] = useState<number>(1)
+
   const { data } = useQuery({
     queryKey: ['products', queryParams],
     queryFn: () => {
@@ -26,6 +31,7 @@ export default function ProductList() {
             <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
               {data && data.data.data.products.map((product) => <Product product={product} />)}
             </div>
+            <Paginate page={page} setPage={setPage} pageSize={20} />
           </div>
         </div>
       </div>
